0ef2ffe6f4efef38694ec03286423451251531e1
selimtan <stan@bpmgenesis.com>
2022-04-22 18:35:14 +0300

    .

--- src/Application.ts
+++ src/Application.ts
@@ -10,15 +10,12 @@ import { StateService } from './Services/StateService';
 const manifest = require('./manifest');
 
 function make(ctorFun, argsArray) {
-    // New instance attached to the prototype but the constructor
-    // hasn't been called on it.
     const newInstance = Object.create(ctorFun.prototype.constructor.prototype);
     ctorFun.prototype.constructor.apply(newInstance, argsArray);
     return newInstance;
 }
 
-// If you create a utility function to create from instance, then it uses the
-// inherited `constructor` property and your change would affect that.
+
 function makeFromInstance(instance, argsArray) {
     return make(instance.constructor, argsArray);
 }
@@ -49,24 +46,16 @@ export class ProcessMining extends TApplication {
             container.registerInstance('IProjectService_Thread', brokerProjectService);
 
             const fileExprorer = new MainView();
-            /*  const controller = new MainController();
-             controller.SetView(fileExprorer); */
             this.SetMainForm(fileExprorer);
-
-
-
-
-            this.Start();
+            this.Start(); // Uygulamayı başlatıyoruz.
 
             //PMAcademyService.Start();
 
 
             TStorage.SaveFile('/static/tuval-core-wp.js', 'tuval-core-wp').then(() => {
-                // debugger;
                 TStorage.GetFile('tuval-core-wp').then((url) => {
                     // const thread = make(PMThreadWorker, ['/static/index-wp.js']);
                     // container.registerInstance('IProjectService_Thread', thread);
-
 
                     /*  thread.load().then(() => {
                         thread.start();
@@ -75,24 +64,8 @@ export class ProcessMining extends TApplication {
             });
 
         });
-
-
-
-
-
-        /*   TuvalTracker.install({
-             token: 'YOUR_TOKEN_HERE',
-             window: { enabled: true }
-         }); */
-
-        /*
-        TuvalTracker.addMetadata("subscription", "professional");
-        TuvalTracker.addMetadata("has_sourcemaps", "false"); */
-
     }
-
-
 }
-// ModuleLoader.FireModuleLoadedEvent(manifest.application.name, ProcessMining);
+
 
 
