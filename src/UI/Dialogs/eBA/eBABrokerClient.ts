15363adf9f66b0c38a152b49cd708b4cd3611613
selimtan <stan@bpmgenesis.com>
2022-04-22 18:37:14 +0300

    .

--- src/UI/Dialogs/eBA/eBABrokerClient.ts
+++ src/UI/Dialogs/eBA/eBABrokerClient.ts
@@ -32,9 +32,9 @@ export class eBABrokerClient {
     public static async GetProjects(): Promise<IEbaProject[]> {
         return new Promise((resolve, reject) => {
             const form = new FormData();
-            form.append('user','cvx');
-            form.append('password','xcv');
-            form.append('lang','xcv');
+            form.append('user','');
+            form.append('password','');
+            form.append('lang','');
             HttpClient.Post(ConfigService.GetEbaBrokerUrl() + '/process/GetProjects', form)
                 .then(response => {
                     resolve(response.data);
@@ -45,10 +45,10 @@ export class eBABrokerClient {
     public static async GetProcesses(process_name: string): Promise<IEbaProcess[]> {
         return new Promise((resolve, reject) => {
             const form = new FormData();
-            form.append('user','cvx');
-            form.append('password','xcv');
+            form.append('user','');
+            form.append('password','');
             form.append('process_name',process_name);
-            form.append('lang','xcv');
+            form.append('lang','');
             HttpClient.Post(ConfigService.GetEbaBrokerUrl() + '/process/GetProcesses', form)
                 .then(response => {
                     resolve(response.data);
@@ -59,11 +59,11 @@ export class eBABrokerClient {
     public static async GetProcessHistory(process_id: string): Promise<any> {
         return new Promise((resolve, reject) => {
             const form = new FormData();
-            form.append('user','cvx');
-            form.append('password','xcv');
+            form.append('user','');
+            form.append('password','');
             form.append('user_id','admin');
             form.append('process_id',process_id);
-            form.append('lang','xcv');
+            form.append('lang','');
             HttpClient.Post(ConfigService.GetEbaBrokerUrl() + '/process/GetProcessHistory', form)
                 .then(response => {
                     resolve(response.data);
@@ -74,10 +74,10 @@ export class eBABrokerClient {
     public static async GetProcessAllHistory(process_name: string): Promise<any> {
         return new Promise((resolve, reject) => {
             const form = new FormData();
-            form.append('user','cvx');
-            form.append('password','xcv');
+            form.append('user','');
+            form.append('password','');
             form.append('process_name',process_name);
-            form.append('lang','xcv');
+            form.append('lang','');
             HttpClient.Post(ConfigService.GetEbaBrokerUrl() + '/process/GetProcessAllHistory/json', form)
                 .then(response => {
                     resolve(response.data);
